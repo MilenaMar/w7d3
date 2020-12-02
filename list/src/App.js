a5b4c1d7f3e4a6affe5219e0b3299ff89fbcb4a6
Andre <itstheandre@gmail.com>
2020-12-02 16:59:26 +0100

    COnditional and loops

--- list/src/App.js
+++ list/src/App.js
@@ -7,32 +7,78 @@ console.log("users:", users.length);
 
 // ["UK", "Spain", "Sweeden", "Portugal"]
 
+// const countries = users.map((user) => user.from)
+// const filtered = countries.filter((el, i) => countries.indexOf(el) === i)
+
+// function expects(arg) {}
+
+// const onClick = () => {
+//   return expects("arg");
+// }
+
+const uniqueCountries = [...new Set(users.map((e) => e.from))];
+
 class App extends React.Component {
-  render() {
-    const usersFromGermany = users.filter((user) => {
-      return user.from === "Germany";
+  state = {
+    selected: "",
+  };
+
+  selectNewCountry = (selection) => {
+    this.setState({
+      selected: selection,
     });
+  };
+
+  consoleHi = () => {
+    console.log("Hi");
+  };
+
+  render() {
+    const filteredUsers = users.filter((user) =>
+      user.from.includes(this.state.selected)
+    );
 
     return (
       <div className="App">
         <header className="App-header">
-          {/* {{#each users}} */}
-          {/* <h1>Username: {users[0].username}</h1> [
-          <h1>Username: {users[0].username}</h1>{{/each}} */}
-          {users.map((user) => {
-            return <Usercard key={user.username} user={user} />;
-          })}
-
-          <h4>All the users from Germany</h4>
-          {usersFromGermany.map((user) => {
-            return <Usercard key={user.username} user={user} />;
-          })}
+          <div>
+            <button onClick={() => this.selectNewCountry("")}>
+              All countries
+            </button>
+            {uniqueCountries.map((el) => (
+              <button onClick={() => this.selectNewCountry(el)} key={el}>
+                {el}
+              </button>
+            ))}
+          </div>
+          <div>
+            {filteredUsers.map((user) =>
+              user.age >= 16 ? (
+                <Usercard key={user.catchPhrase} user={user} />
+              ) : (
+                <h1 style={{ color: "red", fontSize: "50px" }}>
+                  {user.username} is a minor. Shoo, leave
+                </h1>
+              )
+            )}
+          </div>
         </header>
       </div>
     );
   }
 }
-// function App() {
+
+function Usercard(props) {
+  return (
+    <div>
+      <h1>
+        I am {props.user.username}{" "}
+        {props.user.from === "Portugal" && "I am Portuguese"}
+      </h1>
+      <h4>And I can{props.user.age >= 16 ? "" : "'t"} drive a car</h4>
+    </div>
+  );
+}
 
 //   return (
 //     <div className="App">
@@ -52,13 +98,5 @@ class App extends React.Component {
 //     </div>
 //   );
 // }
-
-function Usercard(props) {
-  return (
-    <div>
-      <h1>{props.user.username}</h1>
-    </div>
-  );
-}
 
 export default App;
